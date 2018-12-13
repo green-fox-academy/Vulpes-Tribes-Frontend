8aeec24f86cf280a60ec9e90d61d8f9e2420bcc9
Honza Dudek <honza@honzadudek.cz>
2018-12-13 13:17:10 +0100

    created random generator for tokens, styled form buttons

--- src/app/_helpers/mock_backend.interceptor.ts
+++ src/app/_helpers/mock_backend.interceptor.ts
@@ -15,20 +15,34 @@ import {Observable} from 'rxjs';
 export class MockBackend implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any> | HttpHeaderResponse | HttpProgressEvent | HttpResponse<any> | HttpUserEvent<any>>  {
-    if (req.url.endsWith('/login') && (req.method === 'POST')) {
+    if (req.url.endsWith('/login') &&
+       (req.method === 'POST') &&
+       (req.body.username.length > 0 &&
+        req.body.password.length > 0)) {
       console.log('Intercepted', req);
       return new Observable(observer => {
         observer.next(new HttpResponse<any>(
           {body:
                 { id: 1,
-                  tribes_token: '56987'
-                }, status: 200}
+                  tribes_token: this.generateToken()
+                },
+                status: 200
+              }
         ));
         observer.complete();
       });
-    }
+    } else if (req.url.endsWith('/game'))
     return next.handle(req);
 
+  }
+
+  generateToken(): string {
+    let token = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 6; i++) {
+      token += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return token;
   }
 
 }
