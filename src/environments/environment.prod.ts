c149e813e17791689da5e056482119997e220663
Honza Dudek <honza@honzadudek.cz>
2018-12-18 14:24:23 +0100

    fixed environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Endpoints
+  login: '/login',
+  register: '/register',
+  logout: '/logout',
+  root: '/root',
+  game: '/game',
+
+  // Global variables
+  tribes_token: 'tribes_token',
 };
