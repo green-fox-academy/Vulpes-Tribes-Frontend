ff68fa3dc49d64ec5612ea1c8ccb22256a804f4e
Ivana <ivankanik@gmail.com>
2018-12-20 14:34:05 +0100

    Kingdom service

---
+++ src/app/_models/kingdom.model.ts
@@ -0,0 +1,39 @@
+export class Kingdom {
+    id: number;
+    name: string;
+    user_id: number;
+    buildings: [
+      {
+        id: number,
+        type: string,
+        level: number,
+        hp: number,
+        started_at: number,
+        finished_at: number
+      }
+    ];
+    resources: [
+      {
+        type: string,
+        amount: number,
+        generation: number
+      }
+    ];
+    troops: [
+      {
+        id: number,
+        level: number,
+        hp: number,
+        attack: number,
+        defence: number,
+        started_at: number,
+        finished_at: number
+      }
+    ];
+    location: {
+      x: number,
+      y: number
+    }
+  }
+
+
