051195ce541716446d2b84f70873a701a93c76e1
Sam'Bo <herrerasam88@gmail.com>
2019-01-10 04:26:04 -0500

    Some css changes

--- src/app/game/resources/resources.component.ts
+++ src/app/game/resources/resources.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourcesComponent implements OnInit {
 
-
   constructor(private _http: HttpClient) {
   }
 
--- src/app/game/resources/resources.service.ts
+++ src/app/game/resources/resources.service.ts
@@ -1,19 +1,17 @@
-import { Resources } from '../../_models/resources.model';
+import { Observable } from 'rxjs';
+import { Resources } from './../../_models/resources.model';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ResourcesService implements Resources {
-  amount: number;
-  type: string;
-  generation: number;
-  mockDb() {
-    const resources = [
-      this.type = 'food',
-      this.amount = 500,
-      this.generation = 0
-    ];
-  }
-  constructor() { }
+export class ResourcesService {
+
+    resourceInfo: Resources;
+
+    constructor(private resourcesService: ResourcesService) { }
+
+    getResources(): Observable<any> {
+      return this.resourcesService.getResources();
+    }
 }
