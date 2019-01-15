91af8f444fa9bc6cd6ce486d88d8d10207336d96
Honza Dudek <honza@honzadudek.cz>
2019-01-15 10:36:01 +0100

    changed tslint.json to extend AirBnB styleguide

--- src/app/_helpers/interceptors/buildings.interceptor.ts
+++ src/app/_helpers/interceptors/buildings.interceptor.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {BuildingResponseMock} from '../mocks/buildingResponse.mock';
