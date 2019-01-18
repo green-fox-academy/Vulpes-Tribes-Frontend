9573ca8e99afd4e3ad332019aeb0f370a44d9863
Ivana <ivankanik@gmail.com>
2019-01-18 11:41:43 +0100

    Added tests to kingdom service and kingdom settings service

--- src/app/kingdom-settings/kingdom-settings.component.spec.ts
+++ src/app/kingdom-settings/kingdom-settings.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KingdomSettingsComponent } from './kingdom-settings.component';
+import { Component, OnInit } from '@angular/core';
+import {KingdomSettingsService} from '../kingdom-settings/kingdom-settings.service';
+import { Kingdom } from '../_models/kingdom.model';
+import { KingdomService } from '../services/kingdom.service';
 
 describe('SettingsComponent', () => {
   let component: KingdomSettingsComponent;
---
+++ src/app/kingdom-settings/kingdom-settings.service.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from '@angular/core/testing';
+
+import { KingdomService } from '../services/kingdom.service';
+import { KingdomSettingsService} from './kingdom-settings.service';
+import { async } from 'q';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Kingdom } from '../_models/kingdom.model';
+
+
+fdescribe('KingdomSettingsService', () => {
+  let service: KingdomSettingsService;
+  let kingdomService: KingdomService;
+
+  beforeEach(async() => {
+  TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      RouterTestingModule.withRoutes([])
+    ],
+    providers: [
+      KingdomSettingsService
+    ]
+  });
+});
+
+  beforeEach(() => {
+    service = TestBed.get(KingdomSettingsService);
+    kingdomService = TestBed.get(KingdomService);
+});
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return kingdom', () => {
+ //    let kingdom: Kingdom;
+     spyOn(kingdomService, 'getKingdom');
+     service.getSettings();
+     expect(kingdomService.getKingdom).toHaveBeenCalled();
+//    service.getSettings().subscribe(response => {
+//       kingdom = response.body;
+//        expect(kingdom.name).toEqual('My Kingdom');
+  });
+});
+// });
