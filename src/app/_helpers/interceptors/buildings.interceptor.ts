530f5b7218b9c2a54d763f97430a53ae18b96bb8
Honza Dudek <honza@honzadudek.cz>
2019-01-18 16:40:12 +0100

    created endpoints file to store common endpoints

--- src/app/_helpers/interceptors/buildings.interceptor.ts
+++ src/app/_helpers/interceptors/buildings.interceptor.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
 import { BuildingResponseMock } from '../mocks/buildingResponse.mock';
 import { InterceptorUtilities } from '../../_utilities/interceptor.utilities';
-import {connectableObservableDescriptor} from 'rxjs/internal/observable/ConnectableObservable';
+import { ENDPOINTS } from '../../../environments/endpoints';
 
 const utilities = new InterceptorUtilities();
 
@@ -14,7 +13,7 @@ export class BuildingsInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const buildingsMock = new BuildingResponseMock();
     let buildings = buildingsMock.buildings;
-    if (req.url.endsWith(environment.getBuildings)) {
+    if (req.url.endsWith(ENDPOINTS.getBuildings)) {
       if (req.method === 'GET') {
         return utilities.sendResponse({ buildings } , 200);
       } else if (req.method === 'PUT') {
@@ -23,7 +22,7 @@ export class BuildingsInterceptor implements HttpInterceptor {
         const updatedBuildings = buildingsMock.buildings;
         return utilities.sendResponse({ updatedBuildings }, 200);
       }
-    } else if (req.url.endsWith(environment.createBuilding)) {
+    } else if (req.url.endsWith(ENDPOINTS.createBuilding)) {
       const newBuilding = buildingsMock.createBuilding(req.body.type);
       return utilities.sendResponse({ newBuilding }, 200);
     }
---
+++ src/environments/endpoints.ts
@@ -0,0 +1,11 @@
+export const ENDPOINTS = {
+
+  // Endpoints
+  login: '/login',
+  register: '/register',
+  logout: '/logout',
+  root: '/root',
+  game: '/game',
+  getBuildings: '/game/buildings',
+  createBuilding: '/game/buildings-new',
+}
