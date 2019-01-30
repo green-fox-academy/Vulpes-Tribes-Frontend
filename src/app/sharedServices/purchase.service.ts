c97101d8ea143cf9d6179a84a7ec67fadb097900
Honza Dudek <honza@honzadudek.cz>
2019-01-30 13:37:33 +0100

    added availableToPurchase method

--- src/app/sharedServices/purchase.service.ts
+++ src/app/sharedServices/purchase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Resources } from '../_models/resources.model';
 import { ResourcesService } from '../game/resources/resources.service';
 
@@ -16,16 +16,28 @@ export class PurchaseService {
   constructor(private resourcesService: ResourcesService) {
   }
 
-  ableToPurchaseBuilding(buildingType: string, buildingLevel: number): boolean {
-    this.resourcesService.getResources().subscribe(response => this.resources = response);
-    if (buildingType === 'academy') {
-      return (this.resources[1].amount < buildingLevel * 100);
-    } else if (buildingType === 'factory') {
-      return (this.resources[1].amount < buildingLevel * 50);
-    } else if (buildingType === 'mine') {
-      return (this.resources[1].amount < buildingLevel * 50);
-    } else {
-      return false;
-    }
+  ableToPurchaseBuilding(buildingType: string, buildingLevel: number): Observable<any> {
+    return new Observable(observer => {
+      let ableToBuild: boolean;
+      this.resourcesService.getResources().subscribe(response => {
+        this.resources = response;
+        if (buildingType === 'academy') {
+          ableToBuild = this.resources[1].amount > buildingLevel * 100;
+          this.resources[1].amount -= buildingLevel * 100;
+        } else if (buildingType === 'factory') {
+          ableToBuild = (this.resources[1].amount  > buildingLevel * 50);
+          this.resources[1].amount -= buildingLevel * 100;
+
+        } else if (buildingType === 'mine') {
+          ableToBuild = (this.resources[1].amount  > buildingLevel * 50);
+          this.resources[1].amount -= buildingLevel * 100;
+
+        } else {
+          ableToBuild = false;
+        }
+        observer.next(ableToBuild);
+        observer.complete();
+      });
+    });
   }
 }
