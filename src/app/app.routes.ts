487dd3a4ed98e1fdc99d585b2a4085d14b24c4d5
Lukas Rehor <lukas.rehor@email.cz>
2019-01-31 16:21:03 +0100

    menu functionality done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,21 +18,20 @@ const APP_ROUTES: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'logout', redirectTo: 'login', pathMatch: 'full' },
-  // For now, everything is redirected straight to the game, for release, update to welcome screen
-  { path: '**', redirectTo: 'kingdom', pathMatch: 'full' },
-  {
-    path: 'kingdom', component: GameComponent, canActivate: [AuthGuard], children: [
+  { path: 'kingdom', component: GameComponent, canActivate: [AuthGuard], children: [
       { path: 'notifications', component: NotificationsComponent },
       { path: 'settings', component: KingdomSettingsComponent },
       { path: 'resources', component: ResourcesComponent },
-      { path: 'troops', component: TroopsComponent, outlet: 'menu' },
+      { path: 'troops', component: TroopsComponent },
       {
-        path: 'buildings', component: BuildingsComponent, outlet: 'menu', children: [
+        path: 'buildings', component: BuildingsComponent, children: [
           { path: ':id', component: BuildingDetailComponent },
         ],
       },
     ],
   },
+  // For now, everything is redirected straight to the game, for release, update to welcome screen
+  { path: '**', redirectTo: 'kingdom', pathMatch: 'full' },
 ];
 
 @NgModule({
