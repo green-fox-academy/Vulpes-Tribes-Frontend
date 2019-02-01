eca1370c19c79be05f256e5205600289de1f5d55
Sam'Bo <herrerasam88@gmail.com>
2019-02-01 04:10:17 -0500

    Added a new branch

--- src/app/game/resources/resources.component.ts
+++ src/app/game/resources/resources.component.ts
@@ -8,18 +8,22 @@ import { ResourcesService } from './resources.service';
 })
 export class ResourcesComponent implements OnInit {
   constructor(private resourceService: ResourcesService) {}
-  food;
-  money;
+  food = 500;
+  money = 340;
 
   ngOnInit() {
-    this.showResources();
+    // this.showResources();
+    setInterval(() =>
+    (Date.now() <= this.food.finishedAt) ?
+    this.food
   }
 
-  showResources() {
-    this.resourceService.getResources()
-      .subscribe((response) => {
-        this.food = response[0].amount;
-        this.money = response[1].amount;
-      });
+  // showResources() {
+  //   this.resourceService.getResources()
+  //     .subscribe((response) => {
+  //       this.food = response[0].amount;
+  //       this.money = response[1].amount;
+  //     });
   }
-}
+
+
